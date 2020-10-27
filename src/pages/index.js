85ce343924eb5ba2b6e62d36fca10c379b34732e
Joshua B. Turingan <jotu@splitmedialabs.com>
2020-10-27 22:02:21 +0800

    update title and add react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Home from './home'
+import { Helmet } from 'react-helmet'
 import 'styled-components/macro'
 
 const App = () => {
   return (
-    <Layout>
-      <div
-        css={`
-          width: 100%;
-          height: 100vh;
-          font-family: 'Open Sans', sans-serif;
-          overflow: hidden;
-        `}
-      >
-        <Home />
-      </div>
-    </Layout>
+    <>
+      <Helmet>
+        <title>Hooks Presentation</title>
+      </Helmet>
+      <Layout>
+        <div
+          css={`
+            width: 100%;
+            height: 100vh;
+            font-family: 'Open Sans', sans-serif;
+            overflow: hidden;
+          `}
+        >
+          <Home />
+        </div>
+      </Layout>
+    </>
   )
 }
 
